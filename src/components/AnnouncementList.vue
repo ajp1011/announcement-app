e22f297f92ffbe3e7857468c0f995266608bcde4
AJ Perry <ajp1011@gmail.com>
2023-07-26 13:12:58 -0400

    Adding in pagination

--- src/components/AnnouncementList.vue
+++ src/components/AnnouncementList.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
-    <div v-for="announcement in displayAnnouncements" :key="announcement.id">
+    <div v-for="announcement in paginatedAnnouncements" :key="announcement.id">
       <Announcement :announcement="announcement" />
+    </div>
+    <div v-if="isPaginationNeeded">
+      <button @click="loadNextPage">Load More</button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { defineProps, ref } from 'vue';
-// Import the announcement component
+import { defineProps, ref, computed } from 'vue';
 import Announcement from '@/components/Announcement.vue';
 
 const { announcements } = defineProps(['announcements']);
 
-const displayAnnouncements = ref(announcements);
+const page = ref(1);
+const pageSize = 2; // Adjust the number of announcements per page as needed
+
+const paginatedAnnouncements = computed(() => {
+  const startIndex = (page.value - 1) * pageSize;
+  const endIndex = page.value * pageSize;
+  return announcements.slice(startIndex, endIndex);
+});
+
+const isPaginationNeeded = computed(() => {
+  return page.value * pageSize < announcements.length;
+});
+
+const loadNextPage = () => {
+  page.value += 1;
+};
 </script>
